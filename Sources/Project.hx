9b97d49f8274e86e84bbd7e5f1259bd40e89e8cd
benwaah <benwaah@users.noreply.github.com>
2016-05-10 11:48:17 +0200

    Added ability to change hexes orientation with mouse left click

--- Sources/Project.hx
+++ Sources/Project.hx
@@ -3,18 +3,19 @@ package;
 import geom.Hex;
 import helper.HexHelper;
 import kha.Framebuffer;
+import kha.input.Mouse;
 
 class Project {
 	public var hexes:Array<Hex>;
 
 	public function new() {
-		HexHelper.pointyTopped = true;
-
 		hexes = new Array<Hex>();
 		for (i in 0 ... 10) {
 			hexes.push(new Hex(Math.round(Math.random() * 800),
 							   Math.round(Math.random() * 600)));
 		}
+
+		Mouse.get().notify(onMouseDown, null, null, null);
 	}
 
 	public function update():Void {
@@ -28,5 +29,12 @@ class Project {
 			hexes[i].render(g);
 		}
 		g.end();
+	}
+
+	public function onMouseDown(button:Int, x:Int, y:Int) {
+		if (button == 0)
+		{
+			HexHelper.pointyTopped = !HexHelper.pointyTopped;
+		}
 	}
 }
